9809c35941649bce816c5f2299493cc46c69b1bb
Famtosha <artem_kazakovtsev@mail.ru>
2020-05-06 15:11:04 +0500

    Изменил крышу

--- Assets/Scripts/Spectial/RoofController.cs
+++ Assets/Scripts/Spectial/RoofController.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.EventSystems;
+using System;
 
 public class RoofController : MonoBehaviour
 {
-    [SerializeField] private TilemapRenderer RoofRenderer;
+    [SerializeField] private Tilemap Roof;
     [SerializeField] public GameObject Player;
     [SerializeField] private Camera PlayerCamera;
     [SerializeField] private CameraMove ultrashit;
+
+    private bool IsInRoom = false;
+
+    void Update()
+    {
+        float Change = 0f;
+        if (IsInRoom)
+        {
+            Change = -0.1f;
+        }
+        else
+        {
+            Change = +0.1f;
+        }
+
+        Color NewColor = Roof.color;
+        NewColor.a = Mathf.Clamp(NewColor.a + Change, 0, 1);
+
+        Roof.color = NewColor;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == Player.name)
         {
-            RoofRenderer.enabled = false;
+            IsInRoom = true;
             ultrashit.MoveDown();
         }      
     }
@@ -24,7 +45,7 @@ public class RoofController : MonoBehaviour
     {
         if (collision.gameObject.name == Player.name)
         {
-            RoofRenderer.enabled = true;
+            IsInRoom = false;
             ultrashit.MoveUp();
         }         
     }
