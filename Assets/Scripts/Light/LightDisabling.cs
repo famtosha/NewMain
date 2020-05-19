398f6111244d1177eb2e9267d15dfeea35a6d837
pajiloy_borov <pochta123412345@gmail.com>
2020-05-20 01:58:47 +0300

    Добавлена аппаратная поддержка технологии Real-Time Ray-Tracing

---
+++ Assets/Scripts/Light/LightDisabling.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightDisabling : MonoBehaviour
+{
+
+
+	public static UnityEngine.Experimental.Rendering.LWRP.Light2D Flickering;
+	public float minWaitTime;
+	public float maxWaitTime;
+
+	void Start()
+	{
+		Flickering = GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
+		StartCoroutine(Flashing());
+	}
+
+	IEnumerator Flashing()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+			Flickering.enabled = !Flickering.enabled;
+
+		}
+	}
+}
---
+++ Assets/Scripts/Light/LightFlickering.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LightFlickering : MonoBehaviour
+{
+    public new UnityEngine.Experimental.Rendering.LWRP.Light2D light;
+    public float minIntensity = 0f;
+    public float maxIntensity = 1f;
+    [Range(1, 50)]
+    public int smoothing = 5;
+    Queue<float> smoothQueue;
+    float lastSum = 0;
+
+
+
+    public void Reset()
+    {
+        smoothQueue.Clear();
+        lastSum = 0;
+    }
+
+    void Start()
+    {
+        smoothQueue = new Queue<float>(smoothing);
+        
+        if (light == null)
+        {
+            light = GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
+        }
+    }
+
+    void Update()
+    {
+        if (light == null)
+            return;
+
+        while (smoothQueue.Count >= smoothing)
+        {
+            lastSum -= smoothQueue.Dequeue();
+        }
+
+        float newVal = Random.Range(minIntensity, maxIntensity);
+        smoothQueue.Enqueue(newVal);
+        lastSum += newVal;
+              
+        light.intensity = lastSum / (float)smoothQueue.Count;
+    }
+
+}
