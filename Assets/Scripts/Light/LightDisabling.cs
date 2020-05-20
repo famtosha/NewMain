1dd788427ba2f291e4c88a80139d7725924c7300
pajiloy_borov <pochta123412345@gmail.com>
2020-05-21 01:12:54 +0300

    Updated localization files

--- Assets/Scripts/Light/LightDisabling.cs
+++ Assets/Scripts/Light/LightDisabling.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Experimental.Rendering.LWRP;
 
 public class LightDisabling : MonoBehaviour
 {
 
 
-	public static UnityEngine.Experimental.Rendering.LWRP.Light2D Flickering;
+	public static Light2D Flickering;
 	public float minWaitTime;
 	public float maxWaitTime;
 
 	void Start()
 	{
-		Flickering = GetComponent<UnityEngine.Experimental.Rendering.LWRP.Light2D>();
+		Flickering = GetComponent<Light2D>();
 		StartCoroutine(Flashing());
 	}
 
